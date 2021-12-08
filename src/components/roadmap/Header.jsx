55ca9cf67348e39275631d539688ad886c66baac
Kolade Afode <kolade.afode@yahoo.com>
2021-12-08 19:11:54 +0100

    Add header to roadmap page

---
+++ src/components/roadmap/Header.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+import { BiChevronLeft } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
+import { Button } from '../common/ui/Button';
+
+const Container = styled.div`
+  background: #373f68;
+  border-radius: 10px;
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 48px;
+`;
+
+const Alert = styled.div`
+  display: flex;
+  color: #ffffff;
+  flex-direction: column;
+  gap: 8px;
+
+  h3 {
+    padding-right: 16px;
+  }
+`;
+
+const Back = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  cursor: pointer;
+
+  p {
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 20px;
+    color: #ffffff;
+  }
+`;
+
+const Header = () => (
+  <Container>
+    <Alert>
+      <Link to="/">
+        <Back>
+          <BiChevronLeft />
+          <p>Go Back</p>
+        </Back>
+      </Link>
+
+      <h3>Roadmap</h3>
+    </Alert>
+    <Button>+ Add Feedback</Button>
+  </Container>
+);
+
+export default Header;
--- src/pages/Roadmap.jsx
+++ src/pages/Roadmap.jsx
@@ -4,9 +4,11 @@ import { PageGrid, ItemContainer } from '../components/roadmap/Layout';
 import Planned from '../components/roadmap/planned/Planned';
 import InProgress from '../components/roadmap/inProgress/InProgress';
 import Live from '../components/roadmap/live/Live';
+import Header from '../components/roadmap/Header';
 
 const Roadmap = () => (
   <MainLayout>
+    <Header />
     <PageGrid>
       <ItemContainer>
         <Planned />
