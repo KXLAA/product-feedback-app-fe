695cd2213f22ccddb8df56ed5891864af5e369b2
KXLAA <kolade.afode@yahoo.com>
2022-11-03 20:07:51 +0000

    feat: add tailwind colors
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   fontFamily: {
@@ -10,6 +11,31 @@ module.exports = {
   },
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      pink: "#AD1FEA",
+      orange: "#F49F85",
+      blue: {
+        50: "#F7F8FD",
+        100: "#F2F4FE",
+        200: "#647196",
+        300: "#373F68",
+        400: "#3A4374",
+        500: "#4661E6",
+        600: "#62BCFA",
+        DEFAULT: "#647196",
+      },
+      red: {
+        ...colors.rose,
+        DEFAULT: "#D73737",
+      },
+      gray: colors.neutral,
+      green: colors.green,
+      yellow: colors.amber,
+    },
     extend: {},
   },
   plugins: [],
