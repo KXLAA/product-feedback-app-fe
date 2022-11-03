3f47ac34966a1919770b2ec19b8fe795b259376b
KXLAA <kolade.afode@yahoo.com>
2022-11-03 19:19:33 +0000

    feat: improve eslint config and set up git simple hooks
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
+import "../styles/globals.css";
+
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { trpc } from "@/utils/trpc";
-import { MainLayout } from "@/components/layout/MainLayout";
 
-import "../styles/globals.css";
+import { MainLayout } from "@/components/layout/MainLayout";
+import { trpc } from "@/utils/trpc";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
